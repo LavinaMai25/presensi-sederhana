576706890e5d82e734308fe8b04425c06c9b7895
unknown <lavina.safitri5@gmail.com>
2020-11-19 19:53:07 +0700

    membuat login manual

--- resources/views/Template/left_sidebar.blade.php
+++ resources/views/Template/left_sidebar.blade.php
@@ -70,6 +70,12 @@
               </li>
             </ul>
           </li>
+              <li class="nav-item">
+                <a href="{{route('logout')}}" class="nav-link">
+                  <i class="nav-icon fas fa-th"></i>
+                  <p>Logout</p>
+                </a>
+              </li>
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
